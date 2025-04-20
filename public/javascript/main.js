9536e4553a5b06f83675dd1266bc33418b52f818
Xavix404 <xavixeterno@gmail.com>
2025-04-20 19:02:26 -0400

    revision 4

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,11 +1,31 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.add('show')
-    sidebar.removeAttribute('inert')
+const navBar = document.getElementById('navBar');
+const openButton = document.getElementById('openButton')
+
+const media = window.matchMedia("(width < 700px)")
+
+media.addEventListener('change', (e) => updateNavbar(e))
+
+function updateNavbar(e){
+    const isMobile = e.matches
+    console.log(isMobile)
+    if(isMobile){
+        navBar.setAttribute('inert', '')
+    }
+    else{
+        navBar.removeAttribute('inert')
+    }
 }
 
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.classList.remove('show')
-    sidebar.setAttribute('inert', '')
+function shownavBar(){
+    navBar.classList.add('show')
+    navBar.removeAttribute('inert')
+    openButton.setAttribute('aria-expanded', 'true')
 }
+
+function hidenavBar(){
+    navBar.classList.remove('show')
+    navBar.setAttribute('inert', '')
+    openButton.setAttribute('aria-expanded', 'false')
+}
+
+updateNavbar()
